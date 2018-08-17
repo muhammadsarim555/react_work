37c00e01f0c0aa136addcbba70bec2dc2335a972
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-08-17 11:13:51 +0500

    assignment 1

--- assignment_1/src/App.js
+++ assignment_1/src/App.js
@@ -5,18 +5,28 @@ import './App.css';
 class App extends Component {
   render() {
     const name = "Hello World";
-    const obj = {name: "Hello World Object"}
+    const obj = {name: "Hello World Object"};
+    const data = ['We', 'are', 'United'] //Show these in seperate tags
     return (
       <div className="App">
         <ul> 
           <li> {name} </li>
            </ul>
+            <hr/>
 
         <ul> 
           <li>
             {obj.name}
             </li>
         </ul>
+        <hr/>
+        <ol>
+          
+            {data.map((v ,i)=>{
+              // console.log(v);
+              return <li>{v}</li>
+            })}
+          </ol>
 
       </div>
     );
