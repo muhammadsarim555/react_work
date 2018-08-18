9e88aaacca3e40a1c637a01d635d87ed7719d7e7
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-08-18 19:43:03 +0500

    assignment work added

--- assignment_1/src/App.js
+++ assignment_1/src/App.js
@@ -879,13 +879,43 @@ class App extends Component {
 </div>
 
 <div className="column">
-  <div class="card">
+  <div className="card">
     <h3>Card 4</h3>
     <p>Some text</p>
     <p>Some text</p>
   </div>
 </div>
 </div>
+
+
+{/* modal */}
+
+<button type="button" className="btn btn-primary" data-toggle="modal" data-target="#basicExampleModal">
+  Launch demo modal
+</button>
+
+<div className="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
 
 
         <ul> 
