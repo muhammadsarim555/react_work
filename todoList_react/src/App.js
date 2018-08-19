46c3c04bde56c1899dd5c1a6540573072d4c093f
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-08-19 19:35:24 +0500

    Initial Work Added

---
+++ todoList_react/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  
+
+  renderTitle(){
+    return (
+    <div className="App">
+         <div className="form-group">
+        <input type="text" className="form-control" id="title" placeholder="Title"/>
+    </div>
+
+  </div>
+    );
+  }
+  render() {
+
+    return (
+      console.log(this),
+     this.renderTitle()
+    );
+  }
+}
+
+export default App;
