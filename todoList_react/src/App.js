d92278be89512614e6d803efde5d52db458f214f
Muhammad Sarim <muhammadsarim555@gmail.com>
2018-08-21 01:58:03 +0500

    added work

--- todoList_react/src/App.js
+++ todoList_react/src/App.js
@@ -12,48 +12,41 @@ class App extends Component {
     }
   }
 
-  // update(){
-  //   this.setState ({
-  //   userInput:  input,
-  // })
-
-  // }
-  // work(){
-  //   let titleValue = document.getElementById("title").value;
-
-  // }
-
-changeUserInput(e){
+  
+changeUserInput(input){
     this.setState({
-      userInput: e,
-    }, ( ) => console.log(e));
+      userInput: input,
+    }, ( ) => console.log(input));
   }
-addToList(e){
+addToList(input){
   let listArray = this.state.list; 
-  listArray.push(e)
+  listArray.push(input)
+  this.setState({
+    list : listArray,
+    userInput: ""
+  },()=> console.log(this.state.list))
 }
-  renderTitle(){
-
-    return (
-    <div className="App">
-         <h4 className="h4"> To Do List With React</h4>  
-         <div className="form-group">
-        <input type="text" onChange={(input) => this.changeUserInput(input.target.value)} value={this.state.userInput} className="form-control" id="title" placeholder="Title"/>
-        <button id="addbtn" type="button" onClick={()=> this.addToList(this.state.userInput)} className="btn btn-elegant">Add </button>
-    </div>
-
-  </div>
-    );
-  }
+  
   render() {
     
     return (
       // console.log(this),
 <div className="content" >
       {/* {this.renderHeader()}  */}
-     {this.renderTitle()}
+      <div className="App">
+         <h4 className="h4"> To Do List With React</h4>  
+         <div className="form-group">
+        <input type="text" onChange={(e) => this.changeUserInput(e.target.value)} value={this.state.userInput} className="form-control" id="title" placeholder="Title"/>
+        <button id="addbtn" type="button" onClick={()=> this.addToList(this.state.userInput)} className="btn btn-elegant">Add </button>
+        <ul>
+          {this.state.list.map((val) =>  <li>{val}</li> ) }
+           </ul>
+    </div>
+
+  </div>
+    
       </div>
-    );
+  );
   }
 }
 
