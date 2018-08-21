01f3221264dce72d219f7a61de8ae69ecf4ca2f8
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-21 14:17:51 +0500

    added assignment 2

---
+++ assignment_2/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    
+    return (
+      <div className="container">
+      
+        <h1>Hello world!</h1>
+         </div>
+
+      
+    
+          
+    )
+  }
+}
+
+export default App;
