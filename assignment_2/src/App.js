27731d634fc681f75e60275d16d350229672a32a
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-21 14:31:23 +0500

    added assignment 2

--- assignment_2/src/App.js
+++ assignment_2/src/App.js
@@ -2,19 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    
-    return (
-      <div className="container">
+  constructor(){
+    super()
+    this.state = {
+      text: "Hello World!", 
+    }
+  }
+
+  navbar()
+{
+  return(
+    <div id="navbar"> 
+        <h4> React Assignment 2 </h4>
+      </div>
+  );
+}
+
+body(){
+  <div className="container">
       
-        <h1>Hello world!</h1>
+        <h1 style={{textAlign: "center" , backgroundColor: "blue"}}>{this.state.text}!</h1>
          </div>
 
-      
-    
-          
-    )
-  }
+}
+
+
+render() {
+    return(
+      <div>
+
+        {this.navbar()}
+        {this.body()}
+
+        </div>
+    );
+  
+
+
+}
 }
 
 export default App;
