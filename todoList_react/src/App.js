98e4ef5e72d63f1a9f3c671d722e8710c89c4b94
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-22 08:45:53 +0500

    added work

--- todoList_react/src/App.js
+++ todoList_react/src/App.js
@@ -5,10 +5,13 @@ class App extends Component {
   constructor(){
     super()
   
+    var a = localStorage.getItem("todovalue");
     this.state = {
       userInput : "",
       list : [],
+      ls : a,
     }
+console.log(this.state.ls)
   }
 
   
@@ -21,7 +24,7 @@ changeUserInput(input){
   );
   }
 addToList(p){
-  let listArray = this.state.list; 
+  let listArray = this.state.list;
   if(p == "" ){
     // input == "" ?
      alert("Please Write Something")
@@ -29,33 +32,49 @@ addToList(p){
 // : 
 else{
 listArray.push(p)
+localStorage.setItem("todovalue" , listArray) 
+
   this.setState({
     list : listArray,
     userInput: ""
   },()=> console.log(this.state.list))
   }
 }
+
+renderHeader(){
+  return(<div className="App">
+  <h4 className="h4"> To Do List With React</h4>  
+  <div className="form-group">
+ <input type="text" onChange={(e) => this.changeUserInput(e.target.value)} value={this.state.userInput} className="form-control" id="title" placeholder="Title"/>
+ <button id="addbtn" type="button" onClick={()=> this.addToList(this.state.userInput)} className="btn btn-elegant">Add </button>
+ 
+</div>
+
+</div>);
+}
+
+getValue(){
+  var b = localStorage.getItem("todovalue");
+  return(
+    
+<ul style={{ textAlign: 'center' }}>
+<span>
+          {
+            this.state.list.map((val) =>  <li className="list-group-item">{val}</li> ) }
+      </span>     
+           </ul>
+  )
+}
   
   render() {
-    
     return (
       // console.log(this),
 <div className="content" >
-      {/* {this.renderHeader()}  */}
-      <div className="App">
-         <h4 className="h4"> To Do List With React</h4>  
-         <div className="form-group">
-        <input type="text" onChange={(e) => this.changeUserInput(e.target.value)} value={this.state.userInput} className="form-control" id="title" placeholder="Title"/>
-        <button id="addbtn" type="button" onClick={()=> this.addToList(this.state.userInput)} className="btn btn-elegant">Add </button>
-        
-    </div>
-
-  </div>
-  <ul style={{ textAlign: 'center' }}>
-          {this.state.list.map((val) =>  <li>{val}</li> ) }
-           </ul>
+      {this.renderHeader()} 
+      {this.getValue()}
+  
       </div>
-  );
+      );
   }
 }
 
