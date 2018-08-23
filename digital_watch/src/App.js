8a150187883c36c1b184b457a94ff75bf23b15e7
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-23 22:11:33 +0500

    added work

--- digital_watch/src/App.js
+++ digital_watch/src/App.js
@@ -3,10 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  
+      
+
+   
+    
   render() {
+    const time = new Date();
+      let min = time.getMinutes();
+      let sec = time.getSeconds()
+
+      console.log(sec);
+      
     return (
-      <div className="App">
-        
+      <div className="App" onLoad={() => this.getTime()}>
+      
       </div>
     );
   }
