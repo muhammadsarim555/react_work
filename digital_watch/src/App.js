6591526303c5e3b1979e2017cd7103d3a5eddd1b
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-23 22:19:56 +0500

    added work

--- digital_watch/src/App.js
+++ digital_watch/src/App.js
@@ -3,21 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  
+  constructor(){
+    super();
+    this.state= {
+      time : new Date(),
+    }
+  }
       
 
    
     
   render() {
-    const time = new Date();
-      let min = time.getMinutes();
-      let sec = time.getSeconds()
-
-      console.log(sec);
       
     return (
-      <div className="App" onLoad={() => this.getTime()}>
-      
+      <div className="App">
+        {this.state.time.toLocaleString()}
       </div>
     );
   }
