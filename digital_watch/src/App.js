9a68bc1d1feba9b4e8f54ef76cfcf06636544a1b
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-24 16:41:29 +0500

    added wrok

--- digital_watch/src/App.js
+++ digital_watch/src/App.js
@@ -1,24 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(){
-    super();
-    this.state= {
-      time : new Date(),
-    }
+    super()
+    this.state= {time: new Date()}
+    console.log(this.state.time)
+    this.todayTime()
   }
       
+todayTime(){
+setInterval(()=>{
+  this.setState({
+    time : new Date(),
+  })
+} ,1000)
+}
 
+// componentWillCount(){
+//   setInterval(()=>{
+//     this.todayTime()
+//   } , 1000)
+// }
    
     
   render() {
-      
+       const a = "React Digital Watch " 
+
     return (
-      <div className="App">
-        {this.state.time.toLocaleString()}
-      </div>
+         <div className = "content" > 
+      <h3 className="h3">{a}</h3>
+      
+      <h3>{this.state.time.toLocaleTimeString()}</h3>
+       </div>
     );
   }
 }
