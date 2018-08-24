81708692f3e6b2fc600e65420c7f7ed168754294
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-24 18:54:29 +0500

    added regiter wirk

--- register_work/src/App.js
+++ register_work/src/App.js
@@ -3,10 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super()
+
+  }
   render() {
     return (
       <div className="App">
-       
+        <input type="email"   />
       </div>
     );
   }
