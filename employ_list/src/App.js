1e9aa0b7e6298c67347188c800fd05dc55f142ce
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-31 15:47:57 +0500

    added work

--- employ_list/src/App.js
+++ employ_list/src/App.js
@@ -12,25 +12,33 @@ class App extends Component {
     this.state = {
       email : "sarim@gmail.com",
       password  : "sarimsarim",
-      userEmail : ""
+      userText : "",
+      userPassword : "",
     }
   }
     renderUserInput(i){
       // console.log(i.target.value)
-    const userEmail = i.target.value;
+    const userText = i.target.value;
     this.setState ({
-      userEmail ,
+      userText  , 
     })
   }
 
+  renderUserPassword(i){
+    const userPassword = i.target.value;
+    this.setState({
+      userPassword,
+    })
+  }
   renderSubmit(){
 
-    const { email , password , userEmail }= this.state;
-     userEmail === email ? swal("Good job!", "You have been logined!", "success") : console.log("wrong");
+    const { email , password , userPassword , userText}= this.state;
+    userText === email && userPassword === password ? swal("Good job!", "You have been logined!", "success") : console.log("wrong");
      this.setState ({
-      userEmail: ""
+       userText : "",
+      userPassword: ""
      })
-      console.log(userEmail);
+      console.log(userPassword);
     
   }
   render() {
@@ -39,7 +47,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <input  onChange={this.renderUserInput.bind(this)} value={this.state.userEmail} />
+          <input  onChange={this.renderUserInput.bind(this)} value={this.state.userText} />
+          <input onChange={this.renderUserPassword.bind(this)} value={this.state.userPassword} />
           <button onClick={this.renderSubmit.bind(this)}>Submit </button>
         </header>
       
