b8226baafb164f2aaa5373ad4438ec4714371ee4
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-08-31 14:56:20 +0500

    added work

--- employ_list/src/App.js
+++ employ_list/src/App.js
@@ -38,9 +38,7 @@ class App extends Component {
           <input  onChange={this.renderUserInput.bind(this)} value={this.state.userEmail} />
           <button onClick={this.renderSubmit.bind(this)}>Submit </button>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
       </div>
     );
   }
