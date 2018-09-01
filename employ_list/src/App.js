42806c8410c1301cfcbc830cc3674dca95a56193
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-09-01 19:41:06 +0500

    added work

--- employ_list/src/App.js
+++ employ_list/src/App.js
@@ -93,11 +93,11 @@ logOut(){
     <thead>
       <tr>
         <th scope="col">#</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
-        <th scope="col">Heading</th>
+        <th scope="col">First Name</th>
+        <th scope="col">Last Name</th>
+        <th scope="col">Email</th>
+        <th scope="col">Salary</th>
+        <th scope="col">Date</th>
         <th scope="col">Heading</th>
         <th scope="col">Heading</th>
         <th scope="col">Heading</th>
