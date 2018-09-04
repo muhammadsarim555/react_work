f60e05f4c8cd31c47b1f9a99d8836cee0d6473ce
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-09-05 03:17:56 +0500

    Learning Props

--- using_props/src/Component/BrokenBulb/brokenbulb.js
+++ using_props/src/Component/BrokenBulb/brokenbulb.js
@@ -7,28 +7,38 @@ class Bulb extends Component {
   constructor(props){
     super();
     this.state = {
-      email : "admin",
-      password : "admin",
+      email : "",
+      password : "",
     };
-    this.checkingUser = this.checkingUser.bind(this);
+    // this.checkingUser = this.checkingUser.bind(this);
   }
  
-  checkingUser(){
-    // const {email , password} = this.state;
-    // if(email )
-    this.props.work();
-    return(
-      <input placeholder="Email" onChange={this.setState=({email : })} />
-    )
-  }
+  checkingUser(e){
+    
+    const {email , password} = this.state;
+    if(email === "admin" && password === "admin" ){
+      this.props.work()
+    }
+     }
 
+     gettingEmail(e){
+       this.setState({
+         email : e,
+         password : e,
+        })
+        console.log(this.state.email, "email");
+        console.log(this.state.password , "password");
+     }
   render() {
     console.log(this.props)
     return (
       <div className="app"> 
 
       <h3>{this.props.content}  </h3>
-      <button onClick={this.checkingUser} >Next Page </button>
+      <input value={this.state.email} placeholder="Email" onChange={(e) => this.setState({ email: e.target.value })} />
+        <input value={this.state.password} placeholder="Password" onChange={(e) => this.gettingEmail=( e.target.value )} />
+
+      <button onClick={this.checkingUser.bind(this)} >Next Page </button>
       <button onClick={this.props.forFalse}> Logout </button>
       <br/>
       <hr/>
