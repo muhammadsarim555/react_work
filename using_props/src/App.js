d6e38cee7b86cd94582e57936310513e805ff1f1
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-09-05 14:30:19 +0500

    Added Props Wordk

---
+++ using_props/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import logo from './on.webp';
+import './App.css';
+import Bulb from "./Component/BrokenBulb/Broken"
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+        }
+    }
+
+
+
+
+    render() {
+        return (
+            <div className="app">
+
+
+
+            </div>
+        );
+    }
+}
+
+export default App;
