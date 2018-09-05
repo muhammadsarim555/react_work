552a7a5b47bf09f9c01528c530414914bfc4e7bf
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-09-05 15:48:43 +0500

    Added Props Work

---
+++ using_props/src/Component/OffBulb/OffBulb.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+class Break extends Component {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //        condition : "",
+    //     }
+    // }
+
+   renderState(p){
+        this.props.updateState(p);
+    }
+
+
+
+    render() {
+        return (
+            <div className="app">
+                <button onClick={this.renderState.bind(this , "break")}>Break Bulb </button>
+                {/* <button onClick={this.renderState.bind(this , "on")}>On Bulb </button>
+                <button onClick={this.renderState.bind(this , "")}>Off Bulb </button> */}
+                
+                </div>
+        );
+    }
+}
+
+export default Break;
