daa2ab23c7a57481365d554a7d4a94c1f8eaefbc
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-09-07 20:06:52 +0500

    Using firebase

--- using_firebase/src/App.js
+++ using_firebase/src/App.js
@@ -3,71 +3,89 @@ import logo from './logo.svg';
 import './App.css';
 import firebase from 'firebase';
 
-  var config = {
-    apiKey: "AIzaSyCjQj4vJ7xgmKZyR2bxiqsuqcbeqicrbHA",
-    authDomain: "sarimuni-555.firebaseapp.com",
-    databaseURL: "https://sarimuni-555.firebaseio.com",
-    projectId: "sarimuni-555",
-    storageBucket: "sarimuni-555.appspot.com",
-    messagingSenderId: "352387490785"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyCjQj4vJ7xgmKZyR2bxiqsuqcbeqicrbHA",
+  authDomain: "sarimuni-555.firebaseapp.com",
+  databaseURL: "https://sarimuni-555.firebaseio.com",
+  projectId: "sarimuni-555",
+  storageBucket: "sarimuni-555.appspot.com",
+  messagingSenderId: "352387490785"
+};
+firebase.initializeApp(config);
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      inputValue : "",
+      inputValue: "",
       array: [],
     };
   }
 
-  header(){
-    return(
+  header() {
+    return (
       <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        <div> 
-            <input className="input" value={this.state.inputValue} onChange={(e) => this.checking(e.target.value)}/ >
-            <button onClick={this.submit.bind(this)} >Submit </button>
-            <button onClick={this.bringData.bind(this)} >View Data </button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">Welcome to React</h1>
+        <div>
+          <input className="input" value={this.state.inputValue} onChange={(e) => this.checking(e.target.value)} />
+          <button onClick={this.submit.bind(this)} >Submit </button>
+          <button onClick={this.gettingData.bind(this)} >View Data </button>
 
-          </div>
-        </header>
+        </div>
+      </header>
     )
   }
 
-  checking(e){
+  checking(e) {
     // console.log(e);
     this.setState({
-      inputValue : e,
+      inputValue: e,
     })
     // console.log(this.state.inputValue);
   }
 
-  submit(){
-    const {inputValue} = this.state;
+  submit() {
+    const { inputValue } = this.state;
     // console.log(inputValue , "input value ")
     firebase.database().ref("Data/").push(inputValue);
 
     this.setState({
-      inputValue : "",
+      inputValue: "",
     })
   }
 
-bringData(){
-  
-  const {array} = this.state;
-firebase.database().ref("Data/").on("child_added" , (snapshot) => {
-    console.log(snapshot.val())
-  })
+  gettingData() {
+    
+    const { array } = this.state;
+    let gettingArray = [];
+    firebase.database().ref("Data/").on("child_added", (snapshot) => {
+      // console.log(snapshot.val())
+      gettingArray.push(snapshot.val())
+    })
+    // console.log(gettingArray , "gettingArray");
+    this.setState({
+      array :  gettingArray,
+    })
+    
   }
 
+
   render() {
+    const {array} = this.state;
     return (
       <div className="App">
         {this.header()}
-        
+        <ul>
+        {
+      array.map((v,i)=> {
+        // console.log(v , "v for value")
+        return <li>
+          {v}
+          </li>
+      })
+    }
+    </ul>
       </div>
     );
   }
