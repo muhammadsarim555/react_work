df6b34d089ec473e1c379309d98158cea2511f59
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-09-08 14:25:55 +0500

    Todo WIth firebase

--- firebase_todo/src/App.js
+++ firebase_todo/src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    };
+  }
   render() {
     return (
       <div className="App">
@@ -10,9 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
       </div>
     );
   }
