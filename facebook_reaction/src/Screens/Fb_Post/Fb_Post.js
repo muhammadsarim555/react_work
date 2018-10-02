34ef602ad835f83998b8c2d17992aba7dba73cca
muhammadsarim555 <muhammadsarim555@gmail.com>
2018-10-02 23:35:39 +0500

    Assignment 7 work has been Added

---
+++ facebook_reaction/src/Screens/Fb_Post/Fb_Post.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import '../../App.css';
+
+class Fb_Post extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default Fb_Post;
